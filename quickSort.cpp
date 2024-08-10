7e2bbbf54e3397c5e6cad1afe5b0cc6f897a21af
Vlad <vladner80@gmail.com>
2024-08-10 12:28:01 +0300

    added quickSort

---
+++ quickSort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> quickSort(vector<int> nums)
+{
+    int size = nums.size();
+    if (size < 2)
+        return nums;
+
+    int pivot = nums[0];
+    vector<int> less;
+    vector<int> greater;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (nums[i] <= pivot)
+        {
+            less.push_back(nums[i]);
+        }
+        else
+        {
+            greater.push_back(nums[i]);
+        }
+    }
+    less = quickSort(less);
+    greater = quickSort(greater);
+
+    less.push_back(pivot);
+    less.insert(less.end(), greater.begin(), greater.end());
+
+    return less;
+}
+
+int main()
+{
+    vector<int> nums{3, 2, 1, 5, 10};
+    vector<int> res = quickSort(nums);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << endl;
+    }
+
+    return 0;
+}
